d6862cb7c2576239f251a58f4391b9c4690788fe
ksaalltime8 <noureenk27@gmail.com>
2026-05-15 09:56:57 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
-console.log("🔥 FILE IS RUNNING");
+import dotenv from "dotenv";
+dotenv.config();
 
-setTimeout(() => {
-  console.log("⏱ STILL ALIVE AFTER 5s");
-}, 5000);
+import { Client, GatewayIntentBits } from "discord.js";
+
+process.on("uncaughtException", console.error);
+process.on("unhandledRejection", console.error);
+
+console.log("🔥 BOT STARTING...");
+console.log("TOKEN EXISTS:", !!process.env.TOKEN);
+console.log("CLIENT_ID:", process.env.CLIENT_ID);
+console.log("GUILD_ID:", process.env.GUILD_ID);
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds]
+});
+
+client.once("ready", () => {
+  console.log("✅ BOT ONLINE:", client.user.tag);
+});
+
+client.login(process.env.TOKEN);
